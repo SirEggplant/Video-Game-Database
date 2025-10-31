35bff6298e3cc5c9cee6bab779236f6e8d888fb5
jnDarby <jnd6300@g.rit.edu>
2025-10-30 23:31:31 -0400

    New Collaborators File
    Changing the way a connection is made to the database

--- generated_data/contributor/import_contributors_CSV.py
+++ generated_data/contributor/import_contributors_CSV.py
@@ -1,18 +1,30 @@
 import csv
 import uuid
-import psycopg # pyright: ignore[reportMissingImports]
+import psycopg2  # pyright: ignore[reportMissingImports]
 
 def main():
     password = input("Password: ")
-    with psycopg.connect("dbname=p320_46 user=jnd6300 password=" + password + " host=127.0.0.1") as conn:
-        with conn.cursor() as cur:
-            with open('MOCK_DATA.csv', 'r') as f:
-                for line in f:
-                    company_name = line
-                    company_uuid = str(uuid.uuid4())
-                    cur.execute("INSERT INTO contributors (contributor_UUID, contributor_name) VALUES (%s, %s)", (company_uuid, company_name))
-        conn.commit()
+    conn_params = {
+        "dbname": "p320_46",
+        "user": "jnd6300",
+        "password": password,
+        "host": "127.0.0.1",
+        "port": 5432
+    }
 
+    with psycopg2.connect(**conn_params) as conn:
+        with conn.cursor() as cur:
+            with open('MOCK_DATA.csv', newline='') as csvfile:
+                reader = csv.reader(csvfile)
+                for row in reader:
+                    # Assuming company_name is in the first column of each row
+                    company_name = row[0]
+                    company_uuid = str(uuid.uuid4())
+                    cur.execute(
+                        "INSERT INTO contributors (contributor_UUID, contributor_name) VALUES (%s, %s)",
+                        (company_uuid, company_name)
+                    )
+        conn.commit()
 
 if __name__ == "__main__":
     main()
